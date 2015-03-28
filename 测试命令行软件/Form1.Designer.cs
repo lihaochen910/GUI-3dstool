f983d9e9b74926514099bc38fd55c09ff7dc13c0
Mai.Yi <799189814@qq.com>
2015-03-29 02:31:31 +0800

    the GUI for 3dstool

---
+++ 测试命令行软件/Form1.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace 测试命令行软件
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.导入ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.帮助ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.重置ROMToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button7 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Controls.Add(this.textBox6);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("微软雅黑", 10.52308F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 41);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(794, 458);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "准备文件";
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("微软雅黑", 8.861538F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button6.Location = new System.Drawing.Point(734, 418);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(43, 33);
+            this.button6.TabIndex = 17;
+            this.button6.Text = "...";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(78, 418);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(650, 33);
+            this.textBox6.TabIndex = 16;
+            this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("微软雅黑", 10.52308F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label6.Location = new System.Drawing.Point(236, 388);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(316, 27);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "UpdateData.romfs.xorpad文件";
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("微软雅黑", 8.861538F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button5.Location = new System.Drawing.Point(734, 346);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(43, 33);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "...";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(78, 346);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(650, 33);
+            this.textBox5.TabIndex = 13;
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("微软雅黑", 10.52308F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(255, 316);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(271, 27);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Manual.romfs.xorpad文件";
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("微软雅黑", 8.861538F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button4.Location = new System.Drawing.Point(734, 275);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(43, 33);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "...";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(78, 276);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(650, 33);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微软雅黑", 10.52308F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(255, 246);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(281, 27);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Main.exheader.xorpad文件";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("微软雅黑", 8.861538F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button3.Location = new System.Drawing.Point(734, 201);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(43, 33);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(78, 201);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(650, 33);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 10.52308F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(236, 171);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(304, 27);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Main.exefs_norm.xorpad文件";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("微软雅黑", 8.861538F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(734, 130);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(43, 35);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 10.52308F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(265, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(246, 27);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Main.romfs.xorpad文件";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(78, 131);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(650, 33);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.UpArrow;
+            this.textBox1.Location = new System.Drawing.Point(78, 54);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(650, 33);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 10.52308F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(336, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = ".3ds文件";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.导入ToolStripMenuItem,
+            this.帮助ToolStripMenuItem1,
+            this.重置ROMToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(828, 30);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 导入ToolStripMenuItem
+            // 
+            this.导入ToolStripMenuItem.Font = new System.Drawing.Font("Microsoft YaHei UI", 8.861538F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.导入ToolStripMenuItem.Name = "导入ToolStripMenuItem";
+            this.导入ToolStripMenuItem.Size = new System.Drawing.Size(93, 26);
+            this.导入ToolStripMenuItem.Text = "导入ROM";
+            this.导入ToolStripMenuItem.Click += new System.EventHandler(this.导入ToolStripMenuItem_Click);
+            // 
+            // 帮助ToolStripMenuItem1
+            // 
+            this.帮助ToolStripMenuItem1.Name = "帮助ToolStripMenuItem1";
+            this.帮助ToolStripMenuItem1.Size = new System.Drawing.Size(54, 26);
+            this.帮助ToolStripMenuItem1.Text = "帮助";
+            this.帮助ToolStripMenuItem1.Click += new System.EventHandler(this.帮助ToolStripMenuItem1_Click);
+            // 
+            // 重置ROMToolStripMenuItem
+            // 
+            this.重置ROMToolStripMenuItem.Name = "重置ROMToolStripMenuItem";
+            this.重置ROMToolStripMenuItem.Size = new System.Drawing.Size(54, 26);
+            this.重置ROMToolStripMenuItem.Text = "重置";
+            this.重置ROMToolStripMenuItem.Click += new System.EventHandler(this.重置ROMToolStripMenuItem_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("微软雅黑", 12.18462F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button7.Location = new System.Drawing.Point(311, 505);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(175, 54);
+            this.button7.TabIndex = 2;
+            this.button7.Text = "解包！";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(828, 585);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "3dstool解包-GUI";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 导入ToolStripMenuItem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.ToolStripMenuItem 帮助ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 重置ROMToolStripMenuItem;
+    }
+}
+
---
+++ 测试命令行软件/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Resources;
+using System.IO;
+using System.Diagnostics;
+using System.Reflection;
+namespace 测试命令行软件
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        OpenFileDialog OpenFileDialog1 = new OpenFileDialog();
+
+        private void 导入ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog1.InitialDirectory = System.IO.Directory.GetCurrentDirectory();
+            OpenFileDialog1.Filter = "游戏ROM文件(*.3ds)|*.3ds";
+            OpenFileDialog1.RestoreDirectory = true;
+            if (OpenFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = OpenFileDialog1.FileName;
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog1.InitialDirectory = OpenFileDialog1.FileName;
+            OpenFileDialog1.Filter = "xorpad文件(*.xorpad)|*.xorpad";
+            OpenFileDialog1.RestoreDirectory = true;
+            if (OpenFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = OpenFileDialog1.FileName;
+            }
+        }
+
+        private void 帮助ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("需要与3dstool.exe在同一目录下;\n\n特别感谢dnasdw前辈开发出的3dstool工具；\n以及SarvSarv前辈编写的解包教程\n\n\nMai.Yi", "目前只支持解包操作哦！");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog1.InitialDirectory = OpenFileDialog1.FileName;
+            OpenFileDialog1.Filter = "xorpad文件(*.xorpad)|*.xorpad";
+            OpenFileDialog1.RestoreDirectory = true;
+            if (OpenFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox3.Text = OpenFileDialog1.FileName;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog1.InitialDirectory = OpenFileDialog1.FileName;
+            OpenFileDialog1.Filter = "xorpad文件(*.xorpad)|*.xorpad";
+            OpenFileDialog1.RestoreDirectory = true;
+            if (OpenFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox4.Text = OpenFileDialog1.FileName;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog1.InitialDirectory = OpenFileDialog1.FileName;
+            OpenFileDialog1.Filter = "xorpad文件(*.xorpad)|*.xorpad";
+            OpenFileDialog1.RestoreDirectory = true;
+            if (OpenFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox5.Text = OpenFileDialog1.FileName;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog1.InitialDirectory = OpenFileDialog1.FileName;
+            OpenFileDialog1.Filter = "xorpad文件(*.xorpad)|*.xorpad";
+            OpenFileDialog1.RestoreDirectory = true;
+            if (OpenFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox6.Text = OpenFileDialog1.FileName;
+            }
+        }
+
+        private void 重置ROMToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Directory.CreateDirectory("./cci");
+            Directory.CreateDirectory("./cci/cxi0");
+            Directory.CreateDirectory("./cci/cfa1");
+            Directory.CreateDirectory("./cci/cfa7");
+            Directory.CreateDirectory("./cci/cxi0/romfs");
+            Directory.CreateDirectory("./cci/cfa1/romfs");
+            Directory.CreateDirectory("./cci/cfa7/romfs");
+            string t1=textBox1.Text;
+            string t2 = textBox2.Text;
+            string t3 = textBox3.Text;
+            string t4 = textBox4.Text;
+            string t5 = textBox5.Text;
+            string t6 = textBox6.Text;
+            try
+            {
+                Process p = new Process();
+                p.StartInfo.FileName = "cmd.exe";
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.CreateNoWindow = false;
+                p.StartInfo.RedirectStandardInput = true;
+                p.StartInfo.RedirectStandardOutput = false;
+                p.StartInfo.RedirectStandardError = false;
+                p.Start();
+                //p.StandardInput.WriteLine("@echo off");
+                p.StandardInput.WriteLine("cls");
+                //p.StandardInput.WriteLine("从3ds中提取cci文件");
+                p.StandardInput.WriteLine("3dstool -xvt017f cci cci/0.cxi cci/1.cfa cci/7.cfa " + t1 + " --header cci/nccheader.bin");
+
+
+                //p.StandardInput.WriteLine("执行cxi0解包,取得.bin");
+                p.StandardInput.WriteLine("3dstool -xvtf cxi cci/0.cxi --header cci/cxi0/ncchheader.bin --exh cci/cxi0/exh.bin --plain cci/cxi0/plain.bin --exefs cci/cxi0/exefs.bin --romfs cci/cxi0/romfs.bin --exh-xor " + t4 + " --exefs-xor " + t3 + " --romfs-xor " + t2);
+
+
+
+                //p.StandardInput.WriteLine("执行cfa1解包,取得.bin");
+                p.StandardInput.WriteLine("3dstool -xvtf cfa cci/1.cfa --header cci/cfa1/ncchheader.bin --romfs cci/cfa1/romfs.bin --romfs-xor " + t5);
+                //p.StandardInput.WriteLine("执行cfa7解包,取得.bin");
+                p.StandardInput.WriteLine("3dstool -xvtf cfa cci/7.cfa --header cci/cfa7/ncchheader.bin --romfs cci/cfa7/romfs.bin --romfs-xor " + t6);
+                //p.StandardInput.WriteLine("执行0.cxi\romfs.bin和exefs.bin文件的提取");
+                p.StandardInput.WriteLine("3dstool -xvtf romfs cci/cxi0/romfs.bin --romfs-dir cci/cxi0/romfs");
+                p.StandardInput.WriteLine("3dstool -xvtfu exefs cci/cxi0/exefs.bin --exefs-dir cci/cxi/exefs --header cci/cxi0/exefs/ncchheader.bin");
+                //p.StandardInput.WriteLine("执行0.cxi\romfs.bin和exefs.bin文件的提取");
+                p.StandardInput.WriteLine("3dstool -xvtf romfs cci/cfa1/romfs.bin --romfs-dir cci/cfa1/romfs");
+                p.StandardInput.WriteLine("3dstool -xvtf romfs cci/cfa7/romfs.bin --romfs-dir cci/cfa7/romfs");
+                p.StandardInput.WriteLine("echo 提取完成，请按X退出命令行");
+                p.StandardInput.WriteLine("pause");
+                //p.WaitForExit();
+            }
+            catch { MessageBox.Show("貌似3dstool出问题了", "遇到一个错误"); }
+        }
+    }
+}
